9a9bb50886bd649195388da2aa9d7e8c296ee0b2
Abraham Neuwirth <abeneuwirth@gmail.com>
2016-07-06 20:33:41 -0400

    added solutions to stream_stats

--- week4/stream_stats.py
+++ week4/stream_stats.py
@@ -1,20 +1,42 @@
 #!/usr/bin/env python
+# memory intensive version.
 
 import sys
 import fileinput
+
+def is_even(n):
+    return True if n % 2 == 0 else False
+
+def median(lst):
+    lst.sort()
+    if is_even(len(lst)):
+        mid = len(lst) / 2
+        return (lst[mid - 1] + lst[mid]) / 2
+    else:
+        return (lst[(len(lst) / 2)])
+
+def mean(lst):
+    return float(sum(lst))/len(lst)
 
 if __name__ == '__main__':
 
     # check for input filename given as first argument
     if len(sys.argv) < 2:
         sys.stderr.write('reading input from stdin\n')
-
+    stats = {}
     # read input one line at a time
     for line in fileinput.input():
         # split on tab to get the key and value for each line
         fields = line.rstrip('\n').split('\t')
-
+        stats.setdefault(fields[0], []).append(int(fields[1]))
         # update statistics for the group correponding to this key
         # (minimum, median, mean, and maximum)
 
-    # loop over each group and output the group's key and statistics
+    # loop over each group and output the group's key and
+
+    s = ""
+    for key in stats:
+        s += "%s\t%d\t%.2f\t%.2f\t%d\n" % (key, min(stats[key]), median(stats[key]), mean(stats[key]), max(stats[key]))
+    object = open("output.tsv", "w")
+    object.write(s)
+    object.close()
