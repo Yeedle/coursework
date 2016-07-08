cb011c6241bbb93390188671157f26ecb6d92f70
Abraham Neuwirth <abeneuwirth@gmail.com>
2016-07-08 15:19:02 -0400

    fixed bug in stream_stats

--- week4/stream_stats.py
+++ week4/stream_stats.py
@@ -11,7 +11,7 @@ def median(lst):
     lst.sort()
     if is_even(len(lst)):
         mid = len(lst) / 2
-        return (lst[mid - 1] + lst[mid]) / 2
+        return (lst[mid - 1] + lst[mid]) / 2.0
     else:
         return (lst[(len(lst) / 2)])
 
@@ -23,16 +23,16 @@ if __name__ == '__main__':
     # check for input filename given as first argument
     if len(sys.argv) < 2:
         sys.stderr.write('reading input from stdin\n')
-    stats = {}
+    data = {}
     # read input one line at a time
     for line in fileinput.input():
         # split on tab to get the key and value for each line
         fields = line.rstrip('\n').split('\t')
-        stats.setdefault(fields[0], []).append(int(fields[1]))
+        data.setdefault(fields[0], []).append(int(fields[1]))
         # update statistics for the group correponding to this key
         # (minimum, median, mean, and maximum)
 
-    # loop over each group and output the group's key and
+    # loop over each group and output the group's key and statistics
 
-    for key in stats:
-        print "%s\t%d\t%.2f\t%.2f\t%d\n" % (key, min(stats[key]), median(stats[key]), mean(stats[key]), max(stats[key]))
+    for key in data:
+        print "%s\t%d\t%.2f\t%.2f\t%d\n" % (key, min(data[key]), median(data[key]), mean(data[key]), max(data[key]))
